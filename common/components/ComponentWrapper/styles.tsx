0fe9fa0744d645ad9b4bd9c35f4b9158d06c1fb7
olo90632951 <olo90632951@gmail.com>
2022-09-10 23:22:27 +0900

    Refactor: 사이드바 리팩터링

--- common/components/ComponentWrapper/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-type StyledWrapperProps = {
-  [K in 'isOpened']: boolean;
-};
-
-export const StyledWrapper = styled.div<StyledWrapperProps>`
-  margin-left: ${(props) => (props.isOpened ? 200 : 30)}px;
-`;
