da68ee2623e4115ac057a77259543acd08aebed0
olo90632951 <olo90632951@gmail.com>
2022-09-19 17:32:31 +0900

    Design: 해당날짜에 데이터 보여주는 focus컴포넌트 UI 추가

--- components/common/FocusDay/index.tsx
+++ components/common/FocusDay/index.tsx
@@ -1,18 +1,37 @@
 import { StyledFocusDay } from './styles';
 import { FocusDayPropsType } from './types';
+import { FiEdit2 } from 'react-icons/fi';
+import { IoAddCircle, IoClose } from 'react-icons/io5';
 
 const FocusDay = ({ focusDay }: FocusDayPropsType) => {
+  //            <div>바로가기: {scheduleItem.url}</div>
   return (
-    <StyledFocusDay>
-      <div>
-        {focusDay.year}.{focusDay.month}.{focusDay.date}
+    <StyledFocusDay className="focusday">
+      <div className="focusday__header">
+        <span className="focusday__date">
+          {focusDay.year}.{focusDay.month}.{focusDay.date}
+        </span>
+        <button className="focusday__button focusday__addBtn">
+          <IoAddCircle />
+        </button>
       </div>
       <div>
         {focusDay.data.map((scheduleItem: any, scheduleItemIndex: number) => (
-          <div key={scheduleItemIndex}>
-            <div>제목: {scheduleItem.title}</div>
-            <div>내용: {scheduleItem.content}</div>
-            <div>바로가기: {scheduleItem.url}</div>
+          <div
+            className="focusday__scheduleItem"
+            key={scheduleItemIndex}
+            style={{
+              backgroundColor: `${scheduleItem.backgroundColor}`,
+            }}
+          >
+            <div className="focusday__title">{scheduleItem.title}</div>
+            <div className="focusday__content">{scheduleItem.content}</div>
+            <button className="focusday__button focusday__editBtn">
+              <FiEdit2 />
+            </button>
+            <button className="focusday__button focusday__deleteBtn">
+              <IoClose />
+            </button>
           </div>
         ))}
       </div>
--- components/common/FocusDay/styles.tsx
+++ components/common/FocusDay/styles.tsx
@@ -1,7 +1,88 @@
 import styled from '@emotion/styled';
 
 export const StyledFocusDay = styled.div`
-  width: 70%;
+  width: 60%;
   margin: 0 auto;
-  border: 1px solid black;
+  border: 1px solid #d3d3d37a;
+  border-radius: 10px;
+  padding: 20px;
+  .focusday {
+    &__header {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 20px;
+    }
+    &__date {
+      font-size: 16px;
+      font-weight: 700;
+      //text-align: center;
+    }
+    &__button {
+      border: none;
+      background-color: inherit;
+      cursor: pointer;
+    }
+    &__addBtn {
+      font-size: 21px;
+      color: gray;
+    }
+    &__editBtn {
+      position: absolute;
+      top: 20px;
+      right: 35px;
+      font-size: 14px;
+      color: white;
+    }
+    &__deleteBtn {
+      position: absolute;
+      top: 20px;
+      right: 10px;
+      font-size: 14px;
+      color: white;
+    }
+    &__scheduleItem {
+      margin: 10px 0;
+      border-radius: 10px;
+      color: white;
+      font-weight: 700;
+      padding: 10px 60px 10px 10px;
+      position: relative;
+    }
+    &__title {
+      font-size: 15px;
+    }
+    &__content {
+      font-size: 13px;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    .focusday {
+      &__header {
+        margin-bottom: 10px;
+      }
+      &__date {
+        font-size: 13px;
+      }
+      &__addBtn {
+        font-size: 17px;
+      }
+      &__editBtn {
+        font-size: 12px;
+      }
+      &__deleteBtn {
+        font-size: 12px;
+      }
+      &__scheduleItem {
+        margin: 5px 0;
+      }
+      &__title {
+        font-size: 13px;
+      }
+      &__content {
+        font-size: 11px;
+      }
+    }
+  }
 `;
