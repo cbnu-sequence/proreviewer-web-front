50afb7810d7f0d208e7fc5872ea3fd37ee958b8d
olo90632951 <olo90632951@gmail.com>
2022-09-24 23:31:48 +0900

    Feat: axios interceptor 설정
    axios 요청 보내기 직전에 헤더에 accestoken 넣는 로직 추가함

--- apis/axiosClient/index.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-
-const client: AxiosInstance = axios.create();
-
-client.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
-
-export default client;
